a4cb70b99c51f677889c466f196f7db2de047317
harsh-mohite-09 <harsh.mohite009@gmail.com>
2023-03-06 10:22:44 +0530

    Create 1528-shuffle-string.js

---
+++ 1528-shuffle-string.js
@@ -0,0 +1,16 @@
+// 1528. Shuffle String
+
+// You are given a string s and an integer array indices of the same length.
+// The string s will be shuffled such that the character at the ith position moves to indices[i]
+// in the shuffled string.
+// Return the shuffled string.
+
+const restoreString = function (s, indices) {
+  let str = [];
+  for (let i = 0; i < s.length; i++) {
+    str[indices[i]] = s[i];
+  }
+  return str.join("");
+};
+
+console.log(restoreString("codeleet", [4, 5, 6, 7, 0, 2, 1, 3])); // "leetcode"
