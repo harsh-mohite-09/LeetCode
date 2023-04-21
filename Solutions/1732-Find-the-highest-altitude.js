0533596e5d6f0b0faf77b9cc9782c87edf83dec9
harsh-mohite-09 <harsh.mohite009@gmail.com>
2023-04-21 17:00:31 +0530

    create 1732-Find-the-highest-altitude.js

---
+++ Solutions/1732-Find-the-highest-altitude.js
@@ -0,0 +1,36 @@
+// 1732. Find the Highest Altitude
+
+// There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes.
+// The biker starts his trip on point 0 with altitude equal 0.
+
+// You are given an integer array gain of length n where gain[i] is the net gain in
+// altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+
+// Example 1:
+// Input: gain = [-5,1,5,0,-7]
+// Output: 1
+// Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+
+// Example 2:
+// Input: gain = [-4,-3,-2,-1,4,3,2]
+// Output: 0
+// Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
+
+var largestAltitude = function (gain) {
+  let curr = 0;
+  let max = 0;
+  for (let i = 0; i < gain.length; i++) {
+    curr += gain[i];
+    if (curr > max) max = curr;
+  }
+  return max;
+};
+
+console.log(largestAltitude([-5, 1, 5, 0, -7])); // 1
+console.log(largestAltitude([-4, -3, -2, -1, 4, 3, 2])); // 0
+console.log(
+  largestAltitude([
+    44, 32, -9, 52, 23, -50, 50, 33, -84, 47, -14, 84, 36, -62, 37, 81, -36,
+    -85, -39, 67, -63, 64, -47, 95, 91, -40, 65, 67, 92, -28, 97, 100, 81,
+  ])
+); // 781
